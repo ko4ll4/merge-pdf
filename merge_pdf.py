db69b928d6bba74218a3f3ebbcf23b0d696de834
ko4ll4hk <ko4ll4hk@protonmail.com>
2021-08-17 16:34:48 +0800

    for list

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,15 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 from PyPDF2 import PdfFileWriter, PdfFileReader
+ 
  
 def append_pdf(input,output):
     [output.addPage(input.getPage(page_num)) for page_num in range(input.numPages)]
  
-output = PdfFileWriter()
-append_pdf(PdfFileReader(open("a.pdf","rb")),output)
-append_pdf(PdfFileReader(open("b.pdf","rb")),output)
-append_pdf(PdfFileReader(open("c.pdf","rb")),output)
- 
-output.write(open("combined.pdf","wb"))
+
+def filelist(infile):
+    with open(infile, 'r', encoding='utf-8') as f:
+        file_names = f.read().strip().split('\n')
+    return file_names
+
+
+input_file = 'filelist.txt'
+
+
+if __name__ == '__main__':
+    output = PdfFileWriter()
+    files = filelist(input_file)
+    for f in files:
+        append_pdf(PdfFileReader(open(f, "rb")),output)
+    output.write(open("combined.pdf", "wb"))
